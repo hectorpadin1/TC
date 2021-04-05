9bfe44e9c57b16ce7c54c4645799c2280258e95c
the-scripting-boy <hectorpadint00@gmail.com>
2021-04-05 23:54:04 +0200

    ...

--- P1/p1.ml
+++ P1/p1.ml
@@ -106,12 +106,21 @@ let mix_states s1 s2 =
 	in loop s1 s2 (Conjunto [])
 ;;
 
+let mix_states s1 s2 =
+	let rec loop i cc = 
+		if (i=0) then
+			cc
+		else
+			loop (i-1) (agregar (Estado (string_of_int i)) cc)
+	in loop (cardinal s1 + cardinal s2) (Conjunto [])
+;;
+
 let st_state cc = match cc with
 	Conjunto [] -> raise(Not_found)
 	| Conjunto ((state)::tl) -> state
 ;;
 
-let union af1 af2 = match (af1, af2) with
+let af_union af1 af2 = match (af1, af2) with
 	(Af(states1,simb1,i_states1,arcs1,f_states1), Af(states2,simb2,i_states2,arcs2,f_states2)) ->
 		Af (
 			agregar (Estado "0") (mix_states states1, states2),
@@ -124,31 +133,33 @@ let union af1 af2 = match (af1, af2) with
 		)
 ;;
 
-let af_of_er expression = 
+let af_of_er expression =
 	let rec loop (Af(states,simb,i_states,arcs,f_states)) count = function
-		[] -> (Af(states,simb,i_states,arcs,f_states))
-		| Vacio::tl -> (Af(states,simb,i_states,arcs,f_states))
-		| Constante (t)::tl -> 
+		[] -> Af(states,simb,i_states,arcs,agregar (Estado (string_of_int (cardinal states-1))) f_states)
+		| Vacio::tl -> Af(states,simb,i_states,arcs,f_states)
+
+		| Constante (t)::tl ->
 			if (t = Terminal "") then
-				let af = Af(
-					states,
-					agregar t simb,
-					i_states,
-					arcs,
-					agregar (Estado (string_of_int (count))) f_states)
-				in loop af (count) ([Vacio])
+				let af = Af(states, agregar t simb, i_states, arcs, f_states) in loop af tl
 			else
 				let af = Af(
-					agregar (Estado (string_of_int (count+1))) states,
+					agregar (Estado (string_of_int (new_state states))) states,
 					agregar t simb,
 					i_states,
-					agregar (Arco_af(Estado (string_of_int count), Estado (string_of_int (count+1)), t)) arcs,
-					agregar (Estado (string_of_int (count+1))) f_states)
-				in loop af (count+1) ([Vacio])
-		| Union (er1, er2)::tl -> (Af(states,simb,i_states,arcs,f_states))
-		| Concatenacion (er1, er2)::tl -> (Af(states,simb,i_states,arcs,f_states))
-		| Repeticion er::tl -> (Af(states,simb,i_states,arcs,f_states))
-	in loop (Af(Conjunto [Estado "0"], Conjunto [], Estado "0", Conjunto [], Conjunto [])) 0 [expression]
+					agregar (Arco_af(Estado (string_of_int ((cardinal states)-1)), Estado (string_of_int (cardinal states)), t)) arcs,
+					f_states)
+				in loop af tl
+		| Union (er1, er2)::tl -> af_union (loop (tl@[er1;er2]))
+		| Concatenacion (er1, er2)::tl -> loop (Af(states,simb,i_states,arcs,agregar (Estado (string_of_int (cardinal states-1))) f_states)) (tl@[er1;er2])
+		| Repeticion er::tl -> 
+			let af = Af(
+				states,
+				simb,
+				i_states,
+				agregar (Arco_af(Estado (string_of_int (cardinal states)-1)), Estado (string_of_int (cardinal states)), (Terminal ""))) (agregar (Arco_af(Estado (string_of_int ((cardinal states)-1)), Estado (string_of_int (cardinal states)), (Terminal ""))) arcs),
+				agregar (Estado (string_of_int (cardinal states-1))) f_states
+			in loop af (tl@[er])
+	in loop 0 [expression]
 ;;
 
 
@@ -193,7 +204,7 @@ let af_of_er expression =
 				states,
 				simb,
 				i_states,
-				agregar (Arco_af(Estado (string_of_int ((cardinal states)-1)), Estado (string_of_int (cardinal states)), (Terminal ""))) (agregar (Arco_af(Estado (string_of_int ((cardinal states)-1)), Estado (string_of_int (cardinal states)), (Terminal ""))) arcs),
+				agregar (Arco_af(Estado (string_of_int (cardinal states)-1)), Estado (string_of_int (cardinal states)), (Terminal ""))) (agregar (Arco_af(Estado (string_of_int ((cardinal states)-1)), Estado (string_of_int (cardinal states)), (Terminal ""))) arcs),
 				agregar (Estado (string_of_int (cardinal states-1))) f_states
 			in loop af (tl@[er])
 	in loop (Af(Conjunto [Estado "0"], Conjunto [], Estado "0", Conjunto [], Conjunto [])) [expression]
