96a9eb359810ba25c0f1ded49f414ad29e08c558
the-scripting-boy <hectorpadint00@gmail.com>
2021-04-06 17:53:02 +0200

    concatenacion hecha

--- P1/p1.ml
+++ P1/p1.ml
@@ -119,7 +119,14 @@ let a1 = Af (
 	Conjunto [Estado "3"]
 );;
 
+dibuja_af (af_concat a0 a1);;
 dibuja_af (af_union a0 a1);;
+
+#load "talf.cma";;
+open Auto;;
+open Conj;;
+open Ergo;;
+open Graf;;
 
 let mix_states s1 s2 =
 	let rec loop i cc = 
@@ -160,16 +167,22 @@ let af_union af1 af2 = match (af1, af2) with
 		)
 ;;
 
+let get_final_to_ini f_states1 i_state2 = 
+	let rec loop f_states i_state cc = match (f_states) with
+		Conjunto [] -> cc
+		| Conjunto (f_state::tl) -> loop (Conjunto (tl)) i_state (agregar (Arco_af(f_state,i_state, Terminal "")) cc)
+	in loop f_states1 i_state2 (Conjunto [])
+;;
+
 let af_concat af1 af2 = match (af1, af2) with
 	(Af(states1,simb1,i_state1,arcs1,f_states1), Af(states2,simb2,i_state2,arcs2,f_states2)) ->
-		let states = union states1 states2
-		and st = Estado (string_of_int (new_state (union states1 states2)))
+		let states = (union states1 states2)
 		in Af(
-			agregar st states,
+			states,
 			union simb1 simb2,
-			st,
-			agregar (Arco_af(st, i_state2, Terminal "")) (agregar (Arco_af(st, i_state1, Terminal "")) (union arcs1 arcs2)),
-			union f_states1 f_states2 
+			i_state1,
+			union (get_final_to_ini f_states1 i_state2) (union arcs1 arcs2),
+			f_states2 
 		)
 ;;
 
@@ -194,10 +207,12 @@ let af_of_er expression =
 					f_states
 				)
 				in loop af (count+1) tl
-		| Union (er1, er2)::tl -> 
+		| Union (er1, er2)::tl ->
 			let af = loop (Af(states,simb,i_state,arcs,f_states)) (count) (tl@[er1])
 			in af_union af (loop (Af(states,simb,i_state,arcs,f_states)) (get_states af) (tl@[er2]))
-		| Concatenacion (er1, er2)::tl -> loop (Af(states,simb,i_state,arcs,f_states)) (count+1) (tl@[er1;er2])
+		| Concatenacion (er1, er2)::tl ->
+			let af = loop (Af(states,simb,i_state,arcs,f_states)) (count) (tl@[er1])
+			in af_concat af (loop (Af(states,simb,i_state,arcs,f_states)) (get_states af) (tl@[er2]))
 		| Repeticion er::tl -> loop (Af(states,simb,i_state,arcs,f_states)) (count+1) (tl@[er])
 	in loop (Af(Conjunto [Estado "0"], Conjunto [], Estado "0", Conjunto [], Conjunto [])) 0 [expression]
 ;;
@@ -217,9 +232,13 @@ dibuja_af a0;;
 
 (*a.be.be*)
 let concat_compleja = Concatenacion (Concatenacion (Constante (Terminal "a"),(Constante (Terminal "be"))),(Constante (Terminal "be")));;
+dibuja_af (af_of_er concat_compleja);;
 
-let a1 = af_of_er (Union (Constante (Terminal "a"),(Constante (Terminal "be"))));;
-dibuja_af a1;;
+let a2 = af_of_er (Union (Constante (Terminal "a"),(Constante (Terminal "be"))));;
+dibuja_af a2;;
+
+let a3 = af_of_er (Concatenacion (Constante (Terminal "a"),(Union (Constante (Terminal "ce"),(Constante (Terminal "be"))))));;
+dibuja_af a3;;
 
 let rep = Repeticion (Constante (Terminal "a"));;
 
