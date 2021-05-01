a988a67ade57dec34f104c28e740c337aaeffab1
the-scripting-boy <hectorpadint00@gmail.com>
2021-05-01 16:30:08 +0200

    primera funcion

--- P2/p2.ml
+++ P2/p2.ml
@@ -5,7 +5,7 @@ open Ergo;;
 open Graf;;
 
 (*
-Gic (
+let gic = Gic (
 	Conjunto [No_terminal "S"; No_terminal "A"; No_terminal "B"],
 	Conjunto [Terminal "a"; Terminal "b"; Terminal "c"],
 	Conjunto [
@@ -19,16 +19,40 @@ Gic (
 			[Terminal "b"; Terminal "c"; No_terminal "B"]);
 		Regla_gic (No_terminal "B", [])],
 	No_terminal "S")
+
+let gic = Gic (
+	Conjunto [No_terminal "S"; No_terminal "A"; No_terminal "B"],
+	Conjunto [Terminal "a"; Terminal "b"; Terminal "c"],
+	Conjunto [
+		Regla_gic (No_terminal "S", [No_terminal "A"]);
+		Regla_gic (No_terminal "S", [No_terminal "B"]);
+		Regla_gic (No_terminal "A", [Terminal "C"]);
+		Regla_gic (No_terminal "B", []);
+		Regla_gic (No_terminal "C", [])],
+	No_terminal "S");;
+
 *)
 
-let rec print = function
-	[] -> ". \n "
-	| (Terminal(str)::tl) -> str^(print tl)
+let es_fnc (Gic(no_terminales, terminales, reglas, destacado)) = 
+	let rec loop = function
+		[] -> true
+		| (Regla_gic(not_terminal, lst))::tl -> match lst with
+			[] -> loop tl
+			| hd::tail ->
+				if ((List.length tail) = 0) then
+					loop tl 
+				else false
+	in loop (list_of_conjunto reglas)
 ;;
 
 let es_fnc (Gic(no_terminales, terminales, reglas, destacado)) = 
 	let rec loop = function
-		[] -> ""
-		| (Regla_gic(No_terminal(str), lst))::tl -> str^" -> "^(print lst)
+		(Regla_gic(not_terminal, lst))::tl -> match lst with
+			[] -> loop tl
+			| hd::[] -> loop tl
+			| hd::tail -> match (hd,tail) with
+				((No_terminal t1), [(No_terminal t2)]) -> loop tl
+				| _ -> false
+		| _ -> true
 	in loop (list_of_conjunto reglas)
 ;;
