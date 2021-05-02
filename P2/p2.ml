228b108ca1d0baff6733a3780ba7524febf794f5
the-scripting-boy <hectorpadint00@gmail.com>
2021-05-02 19:49:50 +0200

    e1 y principios de e2.

--- P2/p2.ml
+++ P2/p2.ml
@@ -4,55 +4,85 @@ open Conj;;
 open Ergo;;
 open Graf;;
 
-(*
-let gic = Gic (
-	Conjunto [No_terminal "S"; No_terminal "A"; No_terminal "B"],
-	Conjunto [Terminal "a"; Terminal "b"; Terminal "c"],
-	Conjunto [
-		Regla_gic (No_terminal "S", [Terminal "a"; No_terminal "A"]);
-		Regla_gic (
-			No_terminal "A",
-			[Terminal "a"; Terminal "b"; Terminal "c";
-			No_terminal "A"]);
-		Regla_gic (No_terminal "A", [Terminal "b"; No_terminal "B"]);
-		Regla_gic (No_terminal "B",
-			[Terminal "b"; Terminal "c"; No_terminal "B"]);
-		Regla_gic (No_terminal "B", [])],
-	No_terminal "S");;
-
-let gic = Gic (
-	Conjunto [No_terminal "S"; No_terminal "A"; No_terminal "B"],
-	Conjunto [Terminal "a"; Terminal "b"; Terminal "c"],
-	Conjunto [
-		Regla_gic (No_terminal "S", [No_terminal "A"]);
-		Regla_gic (No_terminal "S", [No_terminal "B"]);
-		Regla_gic (No_terminal "A", [Terminal "C"]);
-		Regla_gic (No_terminal "B", []);
-		Regla_gic (No_terminal "C", [])],
-	No_terminal "S");;
-
-*)
+(*EJERCICIO 1*)
 
 let es_fnc (Gic(no_terminales, terminales, reglas, destacado)) = 
 	let rec loop = function
 		[] -> true
 		| (Regla_gic(not_terminal, lst))::tl -> match lst with
 			[] -> loop tl
-			| hd::tail ->
-				if ((List.length tail) = 0) then
-					loop tl 
-				else false
+			| l -> 
+				match l with
+					[Terminal _] -> loop tl
+					| [No_terminal _] -> loop tl
+					| [No_terminal _; No_terminal _] -> loop tl
+					| _ -> false
 	in loop (list_of_conjunto reglas)
 ;;
 
-let es_fnc (Gic(no_terminales, terminales, reglas, destacado)) = 
-	let rec loop = function
-		(Regla_gic(not_terminal, lst))::tl -> match lst with
-			[] -> loop tl
-			| hd::[] -> loop tl
-			| hd::tail -> match (hd,tail) with
-				((No_terminal t1), [(No_terminal t2)]) -> loop tl
-				| _ -> false
-		| _ -> true
-	in loop (list_of_conjunto reglas)
+(*
+	Ejemplos para el ejercicio 1:
+
+	let gic_fnc = gic_of_string "S A B C; a b; S;S -> A B | B C;A -> B A | a;B -> C C | b;C -> A B | a;";;
+	es_fnc gic_fnc;;
+
+	(A -> a B)
+	let gic1 = gic_of_string "S A B; a b c; S;S -> A;S -> B;A -> a B;B -> A B | epsilon;";;
+	es_fnc gic1;;
+
+	(B -> A B A)
+	let gic2 = gic_of_string "S A B; a b c; S;S -> A;S -> B;A -> a;A -> B;B -> A B A | epsilon;";;
+	es_fnc gic2;;
+
+	(A -> a a)
+	let gic3 = gic_of_string "S A B; a b c; S;S -> A;S -> B;A -> a a;A -> B;B -> A B | epsilon;";;
+	es_fnc gic3;;
+
+	let gic = gic_of_string "S A B; a b c; S;S -> a A;A -> a b c A;B -> b c B | epsilon;";;
+	es_fnc gic;;
+*)
+
+
+
+(*EJERCICIO 2*)
+
+let find terminal reglas =
+	let rec loop conj = function
+		[] -> conj
+		| (Regla_gic(not_terminal, lst))::tl -> 
+			if (List.mem terminal lst) then
+				loop (agregar not_terminal conj) tl
+			else
+				loop conj tl
+	in loop (Conjunto []) reglas
 ;;
+
+let cyk inputs (Gic(no_terminales, terminales, reglas, destacado)) =
+	if ((List.length inputs) = 0) then
+		raise(Failure "Input string is empty.")
+	else if not (es_fnc (Gic(no_terminales, terminales, reglas, destacado))) then
+		raise(Failure "GIC is not in FNC.")
+	else
+		let rec loop lst = function
+			[] -> List.rev lst
+			| terminal::tl -> loop ((find terminal (list_of_conjunto reglas))::lst) tl
+		in loop [] inputs
+;;
+
+
+let cyk inputs (Gic(no_terminales, terminales, reglas, destacado)) =
+	if ((List.length inputs) = 0) then
+		raise(Failure "Input string is empty.")
+	else if not (es_fnc gic) then
+		raise(Failure "GIC is not in FNC.")
+	else
+		let rec loop = function
+			[] -> ""
+			| (Terminal t)::tl -> t^(loop tl)
+		in loop inputs
+;;
+
+(*
+	let g = gic_of_string "S A B C; a b; S;S -> A B | B C;A -> B A | a;B -> C C | b;C -> A B | a;";;
+	cyk (cadena_of_string "b b a b") g;;
+*)
