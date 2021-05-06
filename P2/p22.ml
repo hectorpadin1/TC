9185d862a33908d5a63432001e9342166d97b05b
the-scripting-boy <hectorpadint00@gmail.com>
2021-05-06 16:44:43 +0200

    :).

--- P2/p22.ml
+++ P2/p22.ml
@@ -4,64 +4,6 @@ open Conj;;
 open Ergo;;
 open Graf;;
 
-(*EJERCICIO 1*)
-
-let es_fnc (Gic(_, _, reglas, _)) = 
-	let rec loop = function
-		[] -> true
-		| (Regla_gic(_, lst))::tl -> match lst with
-			[] -> loop tl
-			| l -> 
-				match l with
-					[Terminal _] -> loop tl
-					| [No_terminal _] -> loop tl
-					| [No_terminal _; No_terminal _] -> loop tl
-					| _ -> false
-	in loop (list_of_conjunto reglas)
-;;
-
-(*
-	Ejemplos para el ejercicio 1:
-
-	let gic_fnc = gic_of_string "S A B C; a b; S;S -> A B | B C;A -> B A | a;B -> C C | b;C -> A B | a;";;
-	es_fnc gic_fnc;;
-
-	(A -> a B)
-	let gic1 = gic_of_string "S A B; a b c; S;S -> A;S -> B;A -> a B;B -> A B | epsilon;";;
-	es_fnc gic1;;
-
-	(B -> A B A)
-	let gic2 = gic_of_string "S A B; a b c; S;S -> A;S -> B;A -> a;A -> B;B -> A B A | epsilon;";;
-	es_fnc gic2;;
-
-	(A -> a a)
-	let gic3 = gic_of_string "S A B; a b c; S;S -> A;S -> B;A -> a a;A -> B;B -> A B | epsilon;";;
-	es_fnc gic3;;
-
-	let gic = gic_of_string "S A B; a b c; S;S -> a A;A -> a b c A;B -> b c B | epsilon;";;
-	es_fnc gic;;
-*)
-
-
-
-(*EJERCICIO 2*)
-
-(*
-	let g = gic_of_string "S A B C; a b; S;S -> A B a | B C;A -> B A | a;B -> C C | b;C -> A B | a;";;
-	cyk (cadena_of_string "b b a b") g;;
-
-	let g = gic_of_string "S A B C; a b; S;S -> A B | B C;A -> B A | a;B -> C C | b;C -> A B | a;";;
-	cyk (cadena_of_string "b b a b") g;;
-	cyk (cadena_of_string "a a b b b b") g;;
-
-	let g = gic_of_string "S A B C; a b c; S;S -> A B | B C;A -> B A C | a;B -> C C | b;C -> A B | c;";;
-	cyk (cadena_of_string "b b a c") g;;
-
-	let g = gic_of_string "S X Y A B C D; a b c d; S;S -> A X | C Y | C D; X -> S B; Y -> S D; A -> a; B -> b; C -> c; D -> d";;
-	cyk (cadena_of_string "b b a c") g;;
-	cyk (cadena_of_string "b b a c") g;;
-*)
-
 (*
 
 pruebas
@@ -83,8 +25,6 @@ let r = [Regla_gic (No_terminal "S", [No_terminal "A"; No_terminal "B"]);
             Regla_gic (No_terminal "C", [Terminal "a"])];;
 let c = cadena_of_string "b b a b";;
 *)
-
-exception Parsing_Error;;
 
 (*
 # find (Terminal "a") r;;
@@ -122,7 +62,7 @@ let rec geti number = function
 			hd
 		else
 			geti (number-1) tl
-	| _ -> raise Parsing_Error
+	| _ -> raise(Not_found)
 ;;
 
 let getj number lst =
@@ -183,7 +123,7 @@ let loopi n j lst reglas =
 ;;
 
 
-let cyk inputs (Gic(_, _, reglas_gic, destacado)) =
+let cyk inputs (Gic(no_terminales, terminales, reglas_gic, destacado)) =
 	let reglas = list_of_conjunto reglas_gic in
 	let primera_fila = init_list reglas inputs
 	and len = (List.length inputs) in
@@ -192,7 +132,7 @@ let cyk inputs (Gic(_, _, reglas_gic, destacado)) =
 			(loopi len j lst reglas)::lst
 		else
 			loop ((loopi len j lst reglas)::lst) (j+1)
-	in let gic = loop [primera_fila] 2
-	in pertenece destacado (List.hd (geti 1 gic))
+	in loop [primera_fila] 2
 ;;
 cyk (cadena_of_string "b b a b") (gic_of_string "S A B C; a b; S;S -> A B | B C;A -> B A | a;B -> C C | b;C -> A B | a;");;
+
